af2a666fe7929a45a0747b7749e88dd1e1f32db0
Courtney Chao <couchao@ucdavis.edu>
2018-06-13 15:58:11 -0700

    Completed pop, push, and size for functional stack. Completed enqueue, dequeue, and size for functional queue. Temporarily removed functional-shared to commit

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,13 +7,38 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (storage[0] !== undefined) {
+      for (var key in storage) {
+        storage[key + 1] = storage[key];
+        storage[0] = value;
+      }
+    } else {
+      storage[0] = value;
+    }
+
   };
 
   someInstance.dequeue = function() {
+    var length = Object.keys(storage)[Object.keys(storage).length - 1];
+    var deleted = storage[length];
+    delete storage[length];
+    return deleted;
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for (var key in storage) {
+      if (storage.hasOwnProperty(key)) {
+        size++;
+      }
+    }
+    return size;    
   };
 
   return someInstance;
 };
+
+
+
+//
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,3 +1,13 @@
+//inputs: function
+//output: object
+//constraints: none
+//edge cases: none?
+
+//Create an object that adds to the top and removes from the top 
+
+//push : takes in value puts the value on top of the stack
+//pop: removes top value from stack, returns the removed value
+
 var Stack = function() {
   var someInstance = {};
 
@@ -6,13 +16,41 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if (storage[0] !== undefined) {
+      for (var key in storage) {
+        storage[key + 1] = storage[key];
+        storage[0] = value;
+      }
+    } else {
+      storage[0] = value;
+    }
   };
 
   someInstance.pop = function() {
+    var length = Object.keys(storage)[Object.keys(storage).length - 1];
+    var deleted = storage[0];
+    delete storage[0];
+    for (var key in storage) {
+      storage[key - 1] = storage[key];
+    }
+    delete storage[length];
+    return deleted;
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for (var key in storage) {
+      if (storage.hasOwnProperty(key)) {
+        size++;
+      }
+    }
+    return size;
+  
+    //return Object.keys().length;
   };
 
   return someInstance;
 };
+
+//Create an object that has certain characteristics
+//When the object 
